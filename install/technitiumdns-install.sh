2d65a3c30d4f177f930a9403a6a071a646b42ccf
sebastian.biewer <sebastian.biewer@hpe.com>
2024-06-29 11:38:15 +0200

    .

--- install/technitiumdns-install.sh
+++ install/technitiumdns-install.sh
@@ -32,10 +32,6 @@ msg_info "Installing Technitium DNS"
 $STD bash <(curl -fsSL https://download.technitium.com/dns/install.sh)
 msg_ok "Installed Technitium DNS"
 
-msg_info "Mount Technitium Config Folder"
-$STD mount -t nfs 192.168.178.254:/export/data/technitium/config/ /etc/dns
-msg_ok "Mounted Technitium Config Folder"
-
 motd_ssh
 customize
 
